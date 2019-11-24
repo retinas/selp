55868946377eda392758d1a323cc9e62fb6f2ce4
ilkerAtRetinas <45538580+ilkerAtRetinas@users.noreply.github.com>
2019-11-24 11:03:38 +0300

    Formatting

--- Assets/ScienceLab/Saves/flyControllerScript.js
+++ Assets/ScienceLab/Saves/flyControllerScript.js
@@ -8,11 +8,10 @@ function Update ()
 {
     rotationX += Input.GetAxis("Mouse X")*lookSpeed;
     rotationY += Input.GetAxis("Mouse Y")*lookSpeed;
-    rotationY = Mathf.Clamp (rotationY, -90, 90);
+    rotationY = Mathf.Clamp (rotationY, -90, 90);  
     
     transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
-    transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
-    
+    transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);    
     transform.position += transform.forward*moveSpeed*Input.GetAxis("Vertical");
     transform.position += transform.right*moveSpeed*Input.GetAxis("Horizontal");
 
@@ -21,14 +20,12 @@ if(Input.GetKey("space"))
  transform.position += transform.up*moveSpeed;
 }
 
-
-
 if(Input.GetKey("left shift"))
 {
- moveSpeed = 0.4;
+    moveSpeed = 0.4;
 }
 else{
-moveSpeed = 0.1;
+    moveSpeed = 0.1;
 }
 
 }
